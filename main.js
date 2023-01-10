65a584781a54bfeb3194823a75d983db01add1d9
drtripod <kaizansatta03@gmail.com>
2023-01-09 22:45:58 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const forms = (pwShowHide = document.querySelectorAll(".eye-icon"));
+const signUpBtnLink = document.querySelector(".signUpBtn-link");
+const signInBtnLink = document.querySelector(".signInBtn-link");
+const wrapper = document.querySelector(".wrapper");
+
+pwShowHide.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    let pwFields =
+      eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+    pwFields.forEach((password) => {
+      if (password.type === "password") {
+        password.type = "text";
+        eyeIcon.classList.replace("bx-hide", "bx-show");
+        return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+    });
+  });
+});
+
+signUpBtnLink.addEventListener("click", () => {
+  wrapper.classList.toggle("active");
+});
+
+signInBtnLink.addEventListener("click", () => {
+  wrapper.classList.toggle("active");
+});
